922508179d53d2bddebdd42a2f3f1197a6ef9b4f
cr00z <netrebinr@gmail.com>
2019-04-17 17:25:45 +0700

    write basic functional

--- coursera.py
+++ coursera.py
@@ -1,39 +1,112 @@
-from lxml import etree, html
 import requests
+import lxml
+import json
+import openpyxl
+import argparse
+import os
+from bs4 import BeautifulSoup
+from datetime import datetime
+import re
 
 
 def get_courses_list():
     coursera_link = 'https://www.coursera.org/sitemap~www~courses.xml'
-    coursera_xml = requests.get(coursera_link).text.encode('utf-8')
-    coursera_xml_root = etree.fromstring(coursera_xml)
+    coursera_xml = requests.get(coursera_link).content
+    coursera_xml_root = lxml.etree.fromstring(coursera_xml)
     for course_url_tag in coursera_xml_root.getchildren():
         yield course_url_tag.getchildren().pop().text
 
 
 def get_course_info(course_url):
-    # html.parse don't work correctly
-    page = requests.get(course_url).text.encode('utf-8')
-    tree = html.fromstring(page)
-    course_title = tree.xpath(
-        './/*[@id="root"]/div[1]/div/div[1]/div[1]/div[1]/div/div/div[1]/div[2]/h1'
-        )[0].text_content().encode('utf-8')
-    print(course_title)
-    course_language = tree.xpath(
-        './/*[@id="root"]/div[1]/div/div[2]/div/div/div[2]/div[1]/div[2]/div[4]/div[2]/h4'
-        ).pop().text_content().encode('utf-8')
-    print(course_language)
-    course_begin = tree.xpath(
-        './/*[@id="start-date-string"]/span'
-        )#.pop().text_content().encode('utf-8')
-    print(course_begin)
-    # return course_title, course_language, course_begin
+    DAYS_IN_WEEK = 7
+    WEEK_CORRECTION = 1
+
+    course_page = requests.get(course_url)
+    course_soup = BeautifulSoup(course_page.content, 'lxml')
+    try:
+        course_json_str = course_soup.find(
+            'script',
+            attrs={'type': 'application/ld+json'}
+        ).string
+    except AttributeError:
+        return []
+    course_info = json.loads(course_json_str)
+
+    for course_elem in course_info['@graph']:
+        if course_elem['@type'] == 'Product':
+            course_product = course_elem
+        if course_elem['@type'] == 'Course':
+            course_course = course_elem
+
+    try:
+        course_rating = course_product['aggregateRating']['ratingValue']
+    except KeyError:
+        course_rating = None
+
+    try:
+        start_date = datetime.strptime(
+            course_course['hasCourseInstance']['startDate'],
+            '%Y-%m-%d'
+        )
+        end_date = datetime.strptime(
+            course_course['hasCourseInstance']['endDate'],
+            '%Y-%m-%d'
+        )
+        course_duration = (end_date - start_date).days // DAYS_IN_WEEK - WEEK_CORRECTION
+    except KeyError:
+        course_duration = None
+
+    return [
+        course_product['name'],
+        course_course['inLanguage'],
+        course_product['offers']['validFrom'],
+        course_rating,
+        course_duration
+    ]
 
 
-def output_courses_info_to_xlsx(filepath):
-    pass
+def output_course_info_to_xlsx(filepath, course_info):
+    if os.path.exists(filepath):
+        work_book = openpyxl.load_workbook(filepath)
+    else:
+        work_book = openpyxl.Workbook()
+    work_sheet = work_book.active
+    work_sheet.append(course_info)
+    work_book.save(filepath)
+
+
+def get_cmdline_args():
+    parser = argparse.ArgumentParser(
+        description='courses list grabber from Coursera.org'
+    )
+    parser.add_argument('xlsx_path', help='path for result excel file')
+    parser.add_argument(
+        '--limit',
+        type=int,
+        default=20,
+        help='number of courses'
+    )
+    parser.add_argument(
+        '--start',
+        type=int,
+        default=1,
+        help='course to start'
+    )
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
+    args = get_cmdline_args()
+    if not re.fullmatch(r'.*(\.xlsx|\.xlsm|\.xltx|\.xltm)$', args.xlsx_path):
+        exit('Supported formats are: .xlsx,.xlsm,.xltx,.xltm')
+    course_index = 1
     for course_url in get_courses_list():
-        get_course_info(course_url)
-        break
+        if course_index >= args.start:
+            print(course_url, end=' ')
+            course_info = get_course_info(course_url)
+            course_info = [course_index, course_url] + course_info
+            print(*course_info)
+            output_course_info_to_xlsx(args.xlsx_path, course_info)
+        course_index += 1
+        if course_index == args.start + args.limit:
+            break
